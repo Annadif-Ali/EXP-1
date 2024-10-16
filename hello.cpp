38a3ed912523aafb7203d587f4c6fc86b867c890
Annadif-Ali <annadif.ali.btech2023@sitpune.edu.in>
2024-10-16 11:42:44 +0530

    Update hello.cpp

--- hello.cpp
+++ hello.cpp
@@ -4,3 +4,8 @@ int main() {
     std::cout << "Hello, World!" << std::endl;  // Print Hello, World! to the console
     return 0;  // Indicate that the program ended successfully
 }
+
+/*
+Output:
+Hello, World!
+*/
